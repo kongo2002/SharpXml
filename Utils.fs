24f3e4900f2dec3b2f2137092bd8cabdae6e6daf
kongo2002 <kongo2002@googlemail.com>
2012-04-26 21:43:08 +0200

    use SpinWait() in swapRef

--- Utils.fs
+++ Utils.fs
@@ -7,4 +7,6 @@ module Utils =
     let rec swapRef<'T when 'T : not struct> reference newValue =
         let current = !reference
         let result = Interlocked.CompareExchange<'T>(reference, newValue, current)
-        if not (obj.ReferenceEquals(result, current)) then swapRef reference newValue
+        if not (obj.ReferenceEquals(result, current)) then
+            Thread.SpinWait 20
+            swapRef reference newValue
