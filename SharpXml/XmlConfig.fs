ca1efb617feafb57f5ddc2620d9f92d41b0802e9
kongo2002 <kongo2002@googlemail.com>
2012-05-07 22:38:29 +0200

    add WriteXmlHeader setting

--- SharpXml/XmlConfig.fs
+++ SharpXml/XmlConfig.fs
@@ -5,6 +5,7 @@ type XmlConfig private() =
     let mutable includeNullValues = false
     let mutable excludeTypeInfo = true
     let mutable emitCamelCaseNames = false
+    let mutable writeXmlHeader = false
 
     static let mutable instance = lazy(XmlConfig())
     static member Instance with get() = instance.Value
@@ -20,3 +21,7 @@ type XmlConfig private() =
     member x.EmitCamelCaseNames
         with get() = emitCamelCaseNames
         and set(v) = emitCamelCaseNames <- v
+
+    member x.WriteXmlHeader
+        with get() = writeXmlHeader
+        and set(v) = writeXmlHeader <- v
