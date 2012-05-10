85f36500ed7ecae4ee6c08f5aea70fb47ea735b5
kongo2002 <kongo2002@googlemail.com>
2012-05-10 22:02:22 +0200

    XmlConfig: add doc comments

--- SharpXml/XmlConfig.fs
+++ SharpXml/XmlConfig.fs
@@ -1,5 +1,7 @@
 ﻿namespace SharpXml
 
+/// Singleton configuration class containing all
+/// preferences of the XmlSerializer
 type XmlConfig private() =
 
     let mutable includeNullValues = false
@@ -8,20 +10,29 @@ type XmlConfig private() =
     let mutable writeXmlHeader = false
 
     static let mutable instance = lazy(XmlConfig())
+
+    /// Singleton instance
     static member Instance with get() = instance.Value
 
+    /// Whether to include null values in the serialized output
     member x.IncludeNullValues
         with get() = includeNullValues
         and set(v) = includeNullValues <- v
 
+    /// Whether to exclude additional type information for
+    /// dynamic/anonymous types in the serialized output
     member x.ExcludeTypeInfo
         with get() = excludeTypeInfo
         and set(v) = excludeTypeInfo <- v
 
+    /// Whether to convert property names into camel case
+    /// for the serialized output (i.e 'MyValue' -> 'myValue')
     member x.EmitCamelCaseNames
         with get() = emitCamelCaseNames
         and set(v) = emitCamelCaseNames <- v
 
+    /// Whether to include a XML header sequence in the
+    /// serialized output
     member x.WriteXmlHeader
         with get() = writeXmlHeader
         and set(v) = writeXmlHeader <- v
