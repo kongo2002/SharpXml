62472217185c7ab57722049a8e5f4e3543c37760
kongo2002 <kongo2002@googlemail.com>
2012-05-26 23:03:15 +0200

    add SharpXml.Common project

---
+++ SharpXml.Common/XmlElementAttribute.fs
@@ -0,0 +1,24 @@
+﻿namespace SharpXml.Common
+
+open System
+
+[<AttributeUsage(AttributeTargets.Class ||| AttributeTargets.Property)>]
+type XmlElementAttribute() =
+    inherit Attribute()
+
+    let mutable name = Unchecked.defaultof<string>
+    let mutable itemName = Unchecked.defaultof<string>
+    let mutable keyName = Unchecked.defaultof<string>
+
+    member x.Name
+        with get() = name
+        and set(v) = name <- v
+
+    member x.ItemName
+        with get() = itemName
+        and set(v) = itemName <- v
+
+    member x.KeyName
+        with get() = keyName
+        and set(v) = keyName <- v
+
