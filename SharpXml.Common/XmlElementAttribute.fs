e012436ab2f1f10845fcb0552470bb171b1cd8ba
kongo2002 <kongo2002@googlemail.com>
2012-05-27 23:23:54 +0200

    add missing doc to XmlElementAttribute

--- SharpXml.Common/XmlElementAttribute.fs
+++ SharpXml.Common/XmlElementAttribute.fs
@@ -2,6 +2,8 @@
 
 open System
 
+/// Attribute that allows customization of the serialization
+/// and deserialization behavior of the SharpXml.XmlSerializer
 [<AttributeUsage(AttributeTargets.Class ||| AttributeTargets.Property)>]
 type XmlElementAttribute() =
     inherit Attribute()
@@ -11,18 +13,22 @@ type XmlElementAttribute() =
     let mutable keyName = Unchecked.defaultof<string>
     let mutable valueName = Unchecked.defaultof<string>
 
+    /// Name to override the property name
     member x.Name
         with get() = name
         and set(v) = name <- v
 
+    /// Name to override item names of collections
     member x.ItemName
         with get() = itemName
         and set(v) = itemName <- v
 
+    /// Name to override key names of key-value collections
     member x.KeyName
         with get() = keyName
         and set(v) = keyName <- v
 
+    /// Name to override value names of key-value collections
     member x.ValueName
         with get() = valueName
         and set(v) = valueName <- v
