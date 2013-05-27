24c02520e8f4888facf9e1dbfcba99b339b9e9a8
kongo2002 <kongo2002@googlemail.com>
2013-05-27 16:35:38 +0200

    update to version 1.3.0.0

--- SharpXml/AssemblyInfo.fs
+++ SharpXml/AssemblyInfo.fs
@@ -29,8 +29,8 @@ open System.Runtime.InteropServices
 
 [<assembly:ComVisible(false)>]
 
-[<assembly:AssemblyVersion("1.2.0.0")>]
-[<assembly:AssemblyFileVersion("1.2.0.0")>]
+[<assembly:AssemblyVersion("1.3.0.0")>]
+[<assembly:AssemblyFileVersion("1.3.0.0")>]
 
 [<assembly:InternalsVisibleTo("SharpXml.Tests")>]
 
